94cbaf379a9213b27df89c01ff7b9b070362bf24
abhishekparmar1200 <b19065@students.iitmandi.ac.in>
2021-11-14 18:29:11 +0530

    deployment

--- Group10/functions.py
+++ Group10/functions.py
@@ -3,24 +3,25 @@
 def solve(emsList, absList):
 
 
-
-    originalConc = [325, 337, 353, 369, 388, 412,437, 463, 488, 509, 525, 537, 545, 549]
-    
+    #The CO2 concentration values(PPMV) of the period 1970-2100 at an interval of every 10 years taken from IPCC website
+    originalConc = [325, 337, 353, 369, 388, 412,437, 463, 488, 509, 525, 537, 545, 549] 
+    #Converting the values from PPMV to GtC
     for i in range(0, len(originalConc)):
         originalConc[i] = originalConc[i] * 2.13
     
     years = []
-    
-    for year in range(1971, 2110, 10):
+    #year list stores the corresponding year values
+    for year in range(1970, 2110, 10):
         years.append(year)
     
-    totConc = originalConc[3]
-    totConcList = []
-    yearList = []
-    emsListGraph = []
-    absListGraph = []
+    totConc = originalConc[3] #to store the total concentration of CO2 in atmosphere. Initially stores the CO2 conc value for year 2000
+    totConcList = [] #List to store the total conc of CO2 value over the years
+    yearList = [] #list to store years till the current user input
+    emsListGraph = [] #list to store the emission rate values which the user provides
+    absListGraph = [] #list to store the absorption rate values which the user provides
     year = 2001
     
+    #calculating total concentration values for the emission and apsorption values over the current time period
     for ind in range(0, len(emsList)):
         for year1 in range(year, year + 5):
             totConc = totConc + emsList[ind] - absList[ind]
@@ -30,7 +31,6 @@ def solve(emsList, absList):
             yearList.append(year1)
             
         year = year + 5
-        
     return [yearList, emsListGraph, yearList, absListGraph, years, originalConc, yearList, totConcList]
     # cur = int((year - 1970)/10);
     # lowerLim = int(originalConc[cur] - 200)
--- Group10/views.py
+++ Group10/views.py
@@ -1,3 +1,4 @@
+from io import TextIOBase
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import redirect
@@ -24,8 +25,8 @@ def index(request):
 
         form=request.POST
 
-        a=int(form.get('Emissions'))
-        b=int(form.get('Absorptions'))
+        a=float(form.get('Emissions'))
+        b=float(form.get('Absorptions'))
         l1.append(a)
         l2.append(b)
 
@@ -36,17 +37,18 @@ def index(request):
     #co2 current conseta
         yearList, emsListGraph, yearList, absListGraph, years, originalConc, yearList, totConcList=solve(l1,l2)
         cur = int((yearList[-1] - 1970)/10);
+        print(years)
         plot_div1 = plot([Scatter(x=years, y=originalConc,
                             mode='lines', name='test',
-                            opacity=0.8, marker_color='green'),Scatter(x=yearList, y=totConcList,
+                            opacity=1, marker_color='green',fill='tonexty'),Scatter(x=yearList, y=totConcList,
                             mode='lines', name='test',
-                            opacity=0.8, marker_color='Red')],
+                            opacity=1, marker_color='Red',fill='toself')],
                    output_type='div')
 
         plot_div2 = plot([Scatter(x=yearList, y=emsListGraph,
-                            mode='lines', name='test',
+                            mode='lines', name='emission',
                             opacity=0.8, marker_color='green'),Scatter(x=yearList, y=absListGraph,
-                            mode='lines', name='test',
+                            mode='lines', name='absorption',
                             opacity=0.8, marker_color='Red')],
                    output_type='div')
 
@@ -57,22 +59,45 @@ def index(request):
         #                     opacity=0.8, marker_color='Red')],
         #            output_type='div')
 
-        fig = px.bar(x=[1],y = [totConcList[-1]])
 
+        fig = px.line(x=[0.5,1.5],y=[round(originalConc[cur],3),round(originalConc[cur],3)],labels=dict( y="Concentration"))
+       # fig.add_bar(x=fruits, y=[2,1,3], name="Last year")
+        fig.add_bar(x=[1],y = [totConcList[-1]])
+        
         fig.update_traces(marker_color='orange')
-        # fig1=Scatter(x=[0], y=[int(originalConc[cur])],
-        #                     mode='lines', name='test',
-        #                     opacity=0.8, marker_color='Red')
+        fig.update_yaxes(range=[totConcList[-1]-250,totConcList[-1]+250])
+        
 
         plot_div3=plot(fig,output_type='div')
         form=data()
-        print(yearList)
-        return render (request, "1.html",context={'form':form, 'plot_div1': plot_div1,'plot_div2': plot_div2,'plot_div3': plot_div3, 'year1':yearList[0],'year2':yearList[-1], 'co2_con':totConcList[-1]})
+        
+        return render (request, "1.html",context={'form':form, 'plot_div1': plot_div1,'plot_div2': plot_div2,'plot_div3': plot_div3, 'year1':yearList[0],'year2':yearList[-1], 'co2_con':round(totConcList[-1],3),'co2_ab':round(absListGraph[-1],3),'co2_em':round(emsListGraph[-1],3),
+        'goal_con':round(originalConc[cur],3),'goal_up':round(originalConc[cur],3)+0.5, 'goal_down':round(originalConc[cur],3)-0.5})
     else:
         form = data()
 
+        yearList, emsListGraph, yearList, absListGraph, years, originalConc, yearList, totConcList=solve(l1,l2)
+        plot_div1 = plot([Scatter(x=years, y=originalConc,
+                            mode='lines', name='test',
+                            opacity=0.8, marker_color='green'),Scatter(x=yearList, y=totConcList,
+                            mode='lines', name='test',
+                            opacity=0.8, marker_color='Red')],
+                   output_type='div')
+        
+        fig = px.line(x=[0.5,1.5],y=[round(originalConc[3],3),round(originalConc[3],3)],labels=dict( y="Concentration"))
+        fig.add_bar(x=[1],y = [originalConc[3]])
+        
+        fig.update_traces(marker_color='orange')
+        fig.update_yaxes(range=[originalConc[3]-250,originalConc[3]+250])
+        
 
-    return render(request,"1.html", {'form':form})
+        plot_div3=plot(fig,output_type='div')
+       
+
+
+    l1.clear()
+    l2.clear()
+    return render(request,"1.html", context={'form':form, 'plot_div1':plot_div1,'plot_div3':plot_div3,'co2_con':round(originalConc[3],3),'co2_ab':4.077,'co2_em':8.155})
 
 
 def welcome(request):
