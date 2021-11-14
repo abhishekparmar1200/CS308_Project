70ef6aae6434ec6fbecfd9236b70eafa691b0218
abhishekparmar1200 <b19065@students.iitmandi.ac.in>
2021-11-14 18:32:12 +0530

    added static

---
+++ Group10/static/js/scripts.js
@@ -0,0 +1,10 @@
+
+var x = document.getElementById("flag");
+var make = document.getElementById("output");
+
+console.log("Hello-There")
+function changeflag(flag){
+    console.log("CLICKED");
+    x.hidden=false;
+}
+
